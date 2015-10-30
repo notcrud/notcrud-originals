aff19ef428c780aa1cdb9138b9469fcab7e145ca
Rudraksh MK <rudraksh.mk@gmail.com>
2015-10-30 16:32:11 +0530

    Added a route for the Postman writeup.

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ def home_view():
 	return render_template('test.html')
 
 
+@app.route('/postman')
+def postman_view():
+	post_title = 'Postman'
+	post_headline = 'Fixing the API development workflow.'
+	post_thread_url = 'http://notcrud.com/topic/212/postman-fixing-the-api-workflow'
+
+	return render_template('post.html', post_title=post_title, post_headline=post_headline, post_thread_url=post_thread_url)
+
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=9000)
+	app.run(host='0.0.0.0', port=9000, debug=True)
